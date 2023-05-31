ec0f1d68c4cc8d7835a7eb026d6d2db425bc1e78
Paola <paolandrea96@gmail.com>
2023-05-31 11:01:17 -0500

    Script

--- src/data/script.js
+++ src/data/script.js
@@ -1,60 +1,19 @@
-src routs 
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router(`${__dirname}/db.json`);
+const middlewares = jsonServer.defaults();
 
-import React, { useEffect, useState } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Layout from "../Components/Layout/Layout";
-import Home from "../Components/Home/Home";
-import DetalleFuncion from "../Components/DetalleFuncion/DetalleFuncion";
-import SeleccionBoletos from "../Components/SeleccionBoletos/SeleccionBoletos";
-import SeleccionAsientos from "../Components/SeleccionAsientos/SeleccionAsientos";
-import PagoBoletos from "../Components/PagoBoletos/PagoBoletos";
-import TransaccionExitosa from "../Components/TransaccionExitosa/TransaccionExitosa";
-import Tickets from "../Components/Tickets/Tickets";
-import NotFound from "../Components/NotFound/NotFound";
-import { getMovies } from "../services/getMovies";
+const port = process.env.PORT || 3000;
 
-const AppRouter = () => {
-  const [flights, setFlights] = useState([]);
+server.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Headers', '*')
+    next()
+})
 
-  useEffect(() => {
-    getflights()
-      .then((response) => {
-        if (movies.length === 0) {
-            setFlights(response.results);
-        }
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, [flights]);
+server.use(router);
+server.use(middlewares)
 
-  // const getCurrentMovies = async() => {
-  //   const allMovies = await getMovies();
-  //   setMovies(allMovies);
-  // }
-
-  return (
-    <div>
-      <BrowserRouter>
-        <Routes>
-           
-            <Route index element={<Home/>} />
-            <Route path={":pelicula"} element={<DetalleFuncion />}>
-              <Route path="boletos" element={<SeleccionBoletos />} />
-              <Route path="asientos" element={<SeleccionAsientos />} />
-              <Route path="pagos" element={<PagoBoletos />} />
-              <Route
-                path="transaccionExitosa"
-                element={<TransaccionExitosa />}
-              />
-              <Route path="entradas" element={<Tickets />} />
-            </Route>
-          </Route>
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
-};
-
-export default AppRouter;
+server.listen(port, () => {
+    console.log('JSON Server is running')
+})
